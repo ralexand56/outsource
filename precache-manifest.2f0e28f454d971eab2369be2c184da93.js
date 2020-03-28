c0f56093345f0c2213f5dca482fb6ba85370539e
ralexand56 <ralexand56@live.com>
2020-03-28 10:27:43 -0700

    Updates

---
+++ precache-manifest.2f0e28f454d971eab2369be2c184da93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbccc5111714020c08b9229bffe7335",
+    "url": "/outsource/index.html"
+  },
+  {
+    "revision": "f7efd25181ad41c9c752",
+    "url": "/outsource/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d31990673cb10c24c495",
+    "url": "/outsource/static/js/2.b876efab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/outsource/static/js/2.b876efab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7efd25181ad41c9c752",
+    "url": "/outsource/static/js/main.e751ffc1.chunk.js"
+  },
+  {
+    "revision": "32ca1889a6f497f83876",
+    "url": "/outsource/static/js/runtime-main.f5a2a1e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/outsource/static/media/logo.5d5d9eef.svg"
+  }
+]);
